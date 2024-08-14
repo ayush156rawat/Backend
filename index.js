0d634b686401edb8a614d7da3b18c9df04e7feda
Ayush Rawat <97330088+ayush156rawat@users.noreply.github.com>
2024-08-14 14:14:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use('/uploads',express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials : true,
-  origin: 'https://dezaa.netlify.app/',
+  origin: 'http://localhost:5173',
 }));
 
 
